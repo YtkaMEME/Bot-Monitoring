e3032c4f26c677eb515cbf0be1d40e7f3b6efeb9
Miroslav Muratov <mira@Miroslavs-MacBook-Air.local>
2026-06-02 01:18:00 +0300

    F

--- src/bot/bot_instance.py
+++ src/bot/bot_instance.py
@@ -1,8 +1,8 @@
 from aiogram import Bot
 from config.config import config
 from aiogram.client.session.aiohttp import AiohttpSession
-# from PROXY import PROXY
+from PROXY import PROXY
 # Инициализация бота с токеном из конфигурации
-# session = AiohttpSession(proxy=PROXY)
-# bot = Bot(token=config.token, session=session)
-bot = Bot(token=config.token)
+session = AiohttpSession(proxy=PROXY)
+bot = Bot(token=config.token, session=session)
+# bot = Bot(token=config.token)
